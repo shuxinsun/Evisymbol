431c7fd4ced4dfe592c17d5102ce18a696bdac8c
ssx <152255766+shuxinsun@users.noreply.github.com>
2026-06-26 11:59:19 +0800

    Add files via upload

---
+++ reassessor/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='reassessor',
+    version='1.0.0',
+    author='Hyungseok Kim',
+    description='Reassessor',
+    packages=['reassessor', 'reassessor.lib', 'reassessor.normalizer', 'reassessor.differ'],
+)
